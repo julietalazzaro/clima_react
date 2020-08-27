5f6404da78c0d43e2d085414fd3b2d914e561021
Julieta Lazzaro <julietalazzaro@gmail.com>
2020-08-27 08:12:39 -0300

    Armado del formulario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Header.js";
+import Formulario from "./components/Formulario.js";
 
 function App() {
-  return <h1>Clima React</h1>;
+  return (
+    <Fragment>
+      <Header titulo="Clima React" />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
